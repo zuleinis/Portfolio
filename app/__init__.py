2518a25700d368f109c3f507b436608d89785f76
Zuleinis <zuleinis.ramos@upr.edu>
2021-07-01 21:01:55 +0000

    Week3 changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ import json
 from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.db import get_db
 
 load_dotenv()
 app = Flask(__name__)
@@ -61,3 +63,57 @@ def health():
         side2="Accomplishments",
         url=os.getenv("URL"),
     )
+
+@app.route("/register", methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## to-do: Return a restister page
+    return "Register Page not yet implemented", 501
+
+@app.route("/login", methods=('GET' , 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## to-do: Return a login page
+    return "Login Page not yet implemented", 501
