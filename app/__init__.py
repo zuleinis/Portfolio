6b08cbffceba5460339fe96d4458a1d7778745e0
Zuleinis <zuleinis.ramos@upr.edu>
2021-07-05 19:33:06 +0000

    PostgreSQL working successfully!

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,7 @@ import os
 import json
 from flask import Flask, render_template, send_from_directory, request
 from dotenv import load_dotenv
-# from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
-# from app.db import get_db
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -81,56 +79,49 @@ def accomplishments():
 def health():
     return "Works"
 
-@app.route("/register", methods=('GET', 'POST'))
+@app.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
 
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
-            db.commit()
+            new_user = UserModel(username, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
             return f"User {username} created successfully"
         else:
             return error, 418
 
-    ## to-do: Return a restister page
+    # To-do: Return a restister page
     return "Register Page not yet implemented", 501
 
-@app.route("/login", methods=('GET' , 'POST'))
+
+@app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-        db = get_db()
         error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
+        user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
+        elif not check_password_hash(user.password, password):
             error = 'Incorrect password.'
 
         if error is None:
-            return "Login Successful", 200 
+            return "Login Successful", 200
         else:
             return error, 418
-    
-    ## to-do: Return a login page
+
+    # To-do: Return a login page
     return "Login Page not yet implemented", 501
